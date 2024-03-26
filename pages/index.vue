d39b8c2149f8c4f3715f80f8072204ea125eb62b
zainal umari <zainalumari@pusri.co.id>
2024-03-26 11:21:25 +0700

    add ui

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,41 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h2>Persegi Panjang Area Calculator</h2>
+    <div>
+      <label for="width">Lebar:</label>
+      <input type="string" id="width" v-model="width" />
+    </div>
+    <div>
+      <label for="length">Panjang:</label>
+      <input type="string" id="length" v-model="length" />
+    </div>
+    <button @click="calculate">Hitung Luas</button>
+    <div v-if="calculated">
+      <div v-if="errorMessage">{{ errorMessage }}</div>
+      <div v-else>Luas Persegi Panjang: {{ area }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
+const {calculateArea, inputValidation} = require("../functions/rectangle");
+
 export default {
-  name: 'IndexPage'
+  data() {
+    return {
+      width: null,
+      length: null,
+      area: null,
+      calculated: false,
+      errorMessage: ''
+    };
+  },
+  methods: {
+    calculate() {
+      this.calculated = true;
+      this.errorMessage = inputValidation(this.width, this.length);
+      this.area = calculateArea(this.width, this.length);
+    }
+  }
 }
 </script>
